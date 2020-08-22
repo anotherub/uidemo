4d319e6c653d539fb1d0454cf6839d141b4156d3
umeshbhat <umeshbhat30@gmail.com>
2020-08-22 20:26:43 +0530

    Updates

---
+++ precache-manifest.2596e5da72494f94680af793da0d4741.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c3e38bc6a45646c002ba37de9efd36",
+    "url": "/uidemo/index.html"
+  },
+  {
+    "revision": "30c5fe4a3dd9169df374",
+    "url": "/uidemo/static/css/main.1378b27d.chunk.css"
+  },
+  {
+    "revision": "51304e4cb3deaebf64ae",
+    "url": "/uidemo/static/js/2.b182ac44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/uidemo/static/js/2.b182ac44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c5fe4a3dd9169df374",
+    "url": "/uidemo/static/js/main.bb4fc8ba.chunk.js"
+  },
+  {
+    "revision": "86ed1b18a2e2dce92ee4",
+    "url": "/uidemo/static/js/runtime-main.c18e5efd.js"
+  },
+  {
+    "revision": "da5fc8771a94ce75c3962b89b380c672",
+    "url": "/uidemo/static/media/background.da5fc877.jpg"
+  },
+  {
+    "revision": "13f9220d8cb648466f46a8b7083bf055",
+    "url": "/uidemo/static/media/bg-learn.13f9220d.png"
+  },
+  {
+    "revision": "bd62729c17a3744082f52b7c48ba9c35",
+    "url": "/uidemo/static/media/board-notes.bd62729c.png"
+  }
+]);
